17ebee45abe282a388374d920e00b993d0e8baae
Tim <tim.jouliab@icloud.com>
2023-11-18 19:23:52 +0100

    feat: added a module for translation

--- le-grenier-front/src/app/app.component.ts
+++ le-grenier-front/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'le-grenier';
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('fr');
+    translate.use('fr');
+  }
 }
--- le-grenier-front/src/app/app.module.ts
+++ le-grenier-front/src/app/app.module.ts
@@ -1,12 +1,33 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/modules/shared.module';
+import { RouterModule } from '@angular/router';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
 @NgModule({
   declarations: [AppComponent],
-  imports: [SharedModule, MatListModule, MatToolbarModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatListModule,
+    MatToolbarModule,
+    BrowserModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
