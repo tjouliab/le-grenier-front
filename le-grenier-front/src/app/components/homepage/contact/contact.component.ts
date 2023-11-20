876519a9b03fb6b65a909e82cfb367c85c93b098
Tim <tim.jouliab@icloud.com>
2023-11-20 20:43:10 +0100

    feat: improve look of form

--- le-grenier-front/src/app/components/homepage/contact/contact.component.ts
+++ le-grenier-front/src/app/components/homepage/contact/contact.component.ts
@@ -1,13 +1,44 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    TranslateModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
+  contactForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    subject: new FormControl('', [Validators.required]),
+    message: new FormControl('', [Validators.required]),
+  });
 
+  constructor(private translate: TranslateService) {}
+
+  ngOnInit() {}
+
+  submitForm(): void {
+    console.log('submitFormValue', this.contactForm.value);
+    console.log('submitFormValid', this.contactForm.valid);
+  }
 }
