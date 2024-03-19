fed72e2021acba11d35aafeb98f12c6cfa236af1
tjouliab <tim.jouliab@icloud.com>
2024-03-19 17:36:58 +0100

    feat: delete unused features

--- src/environment.ts
+++ src/environment.ts
@@ -1,4 +1,2 @@
 export const ADDRESS = '1 Rue du Vieux-Marché-aux-Poissons, 67000, Strasbourg';
 export const CONTACT_EMAIL = 'legrenier.camping@gmail.com';
-export const CONTACT_PHONE = '07 49 95 90 24';
-export const TRADEMARK = '2023 Le Grenier';
