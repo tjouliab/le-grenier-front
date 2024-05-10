cf2c8238d54a857a2483893c44de11048a2bae4e
Tim <tim.jouliab@icloud.com>
2024-05-10 18:34:18 +0200

    feat: implement home's services

--- src/app/components/homepage/accueil/accueil.component.ts
+++ src/app/components/homepage/accueil/accueil.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-accueil',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule, TranslateModule],
   templateUrl: './accueil.component.html',
-  styleUrl: './accueil.component.scss'
+  styleUrl: './accueil.component.scss',
 })
-export class AccueilComponent {
-
-}
+export class AccueilComponent {}
